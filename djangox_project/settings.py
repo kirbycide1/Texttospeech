992d756d0ce3f1731b750f4977bd214d1b400b9d
Your Name <allmyworkcomeshere@gmail.com>
2020-04-07 16:31:09 +0800

    Finihsed models

--- djangox_project/settings.py
+++ djangox_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'texttwospeech.apps.TexttwospeechConfig',
+   
 
     # Third-party
     'allauth',
---
+++ texttwospeech/models.py
@@ -0,0 +1,18 @@
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+from django.conf import settings
+from django.utils import timezone 
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    settings = JSONField()
+
+class Paragraphs(models.Model):
+    Project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    audio = models.FileField()
+    order = models.IntegerField()
